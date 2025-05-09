8f1fcb1a83c35a51cfefe051ce9ba1cf7877323f
Alex Higham <117798097+alex-higham@users.noreply.github.com>
2025-05-08 19:40:39 -0700

    Update astro.config.mjs
    modified to use my name instead of repository author

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
-import prefetch from "@astrojs/prefetch";
+import tailwind from "@alex-higham/tailwind";
+import sitemap from "@alex-higham/sitemap";
+import prefetch from "@alex-higham/prefetch";
 
 export default defineConfig({
   integrations: [tailwind(), sitemap(), prefetch()],
-  site: "https://adulbrich.github.io",
+  site: "https://alex-higham.github.io",
   base: "/demo-github-actions",
 });
